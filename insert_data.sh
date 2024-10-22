d8a415db089e313204cd82978458774535ba6d90
helder mabasso <hfmabasso@gmail.com>
2024-10-22 17:22:20 +0200

    Clear all tables before insert .csv data

--- insert_data.sh
+++ insert_data.sh
@@ -3,3 +3,4 @@
 # Script to insert data from courses.csv and students.csv into students database
 
 PSQL="psql -X --username=freecodecamp --dbname=students --no-align --tuples-only -c"
+echo $($PSQL "TRUNCATE students, majors, courses, majors_courses")
