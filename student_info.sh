85001ca86cfd1ba61d385a3b2d3a62d8726161e8
helder mabasso <hfmabasso@gmail.com>
2024-10-23 19:05:33 +0200

    first and last names of students with gpa = 4

--- student_info.sh
+++ student_info.sh
@@ -6,3 +6,7 @@ echo -e "\n~~ My Computer Science Students ~~\n"
 
 #initializing and connecting to students db
 PSQL="psql -X --username=freecodecamp --dbname=students --no-align --tuples-only -c"
+
+# first and last names of students with gpa = 4
+echo -e "\nFirst name, last name, and GPA of students with a 4.0 GPA:"
+echo "$($PSQL "SELECT first_name, last_name, gpa FROM students WHERE gpa = 4.0")"
